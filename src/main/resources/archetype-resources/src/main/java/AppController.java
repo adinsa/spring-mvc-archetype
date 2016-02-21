e7952afe1b7c298842063109c3126e572b5f9695
Amar Dinsa <dinsaa@gmail.com>
2016-02-21 18:00:25 -0500

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/AppController.java
@@ -0,0 +1,20 @@
+package org.github.adinsa.spring_mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(AppController.class);
+
+    @RequestMapping(value = "/hello")
+    public @ResponseBody String helloWorld() {
+        logger.info("In '/hello'");
+        return "Hello, world!";
+    }
+}
